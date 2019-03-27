90f506989499f7436082681193c6e165b86505ba
Alejandro <alejandro.tang@utec.edu.pe>
2019-03-26 21:27:45 -0500

    First working

---
+++ cmake-build-debug/CMakeFiles/the-3n-1-problem-Mandarine2312.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/the-3n-1-problem-Mandarine2312.dir/main.cpp.obj"
+  "the-3n-1-problem-Mandarine2312.pdb"
+  "the-3n-1-problem-Mandarine2312.exe"
+  "the-3n-1-problem-Mandarine2312.exe.manifest"
+  "libthe-3n-1-problem-Mandarine2312.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/the-3n-1-problem-Mandarine2312.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -2,6 +2,30 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int algorithm (int n, int counter){
+    if(n == 1){
+        return counter;
+    }else if (n%2 == 1){
+        return algorithm(3*n+1, counter + 1);
+    }else{
+        return algorithm(n/2, counter + 1);
+    }
+}
+
+int main() {
+    int n1, n2, max, temp;
+    cin >> n1;
+    cin >> n2;
+    max = 0;
+
+    for(int i = n1; i <= n2; i++){
+        temp = algorithm(i, 1);
+        if(temp > max){
+            max = temp;
+        }
+    }
+
+    cout << n1 << " " << n2 << " " << max;
+
     return 0;
 }
